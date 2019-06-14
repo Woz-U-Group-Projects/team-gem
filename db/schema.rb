be15a2682bb279619496336bdbb1d6a16598c80c
Bartschij <bartschij001@gmail.com>
2019-06-14 13:19:47 -0400

    Added Quantity to Shopping list Items

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190526233443) do
+ActiveRecord::Schema.define(version: 20190614164912) do
 
   create_table "shopping_items", force: :cascade do |t|
     t.string   "content"
     t.integer  "shopping_list_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.datetime "purchased_at"
+    t.integer  "quantity",         default: 1
     t.index ["shopping_list_id"], name: "index_shopping_items_on_shopping_list_id"
   end
 
