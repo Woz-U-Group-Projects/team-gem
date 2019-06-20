86773a503773c2b4ac7f1801ae1cdccca5da9d1f
daphneblumen <daphne@tierrait.com>
2019-06-20 00:34:46 -0700

    Added QTY to Shopping Items form and displayed values on items list

--- app/controllers/shopping_items_controller.rb
+++ app/controllers/shopping_items_controller.rb
@@ -32,7 +32,7 @@ class ShoppingItemsController < ApplicationController
   end
 
   def shopping_item_params
-    params[:shopping_item].permit(:content)
+    params[:shopping_item].permit(:content, :quantity)
   end
 
 end
